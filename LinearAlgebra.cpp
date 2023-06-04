fa5b2e6ede340ab0e29d6e55282411662c8aa1ca
Jeremy Blouir <jrmyblouir@gmail.com>
2023-06-04 08:52:39 -0600

    Adding the start of a 3D vector class

--- LinearAlgebra.cpp
+++ LinearAlgebra.cpp
@@ -1,12 +1,44 @@
 
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 using namespace sf;
+
+struct Vector3D
+{
+    float x, y, z;
+
+    Vector3D() = default;
+
+    Vector3D(float a, float b, float c)
+    {
+        x = a;
+        y = b;
+        z = c;
+    }
+
+    float& operator [](int i)
+    {
+        return ((&x)[i]);
+    }
+
+    const float& operator [](int i) const
+    {
+        return ((&x)[i]);
+    }
+};
+
 
 int main()
 {
     VideoMode vm(1920, 1080);
     RenderWindow window(vm, "LinearAlgebra", Style::Fullscreen);
+
+    Vector3D vector(4, 8, 16);
+
+    std::cout << vector[0];
+    std::cout << vector[1];
+    std::cout << vector[2];
 
     while (window.isOpen())
     {
