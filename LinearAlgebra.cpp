2ffdba6f61eab0d9e1ae0177f0ff9f9b3065da0e
Jeremy Blouir <jrmyblouir@gmail.com>
2023-06-04 09:42:46 -0600

    More Vector3D math ops

--- LinearAlgebra.cpp
+++ LinearAlgebra.cpp
@@ -26,8 +26,77 @@ struct Vector3D
     {
         return ((&x)[i]);
     }
+
+    Vector3D& operator *=(float s)
+    {
+        x *= s;
+        y *= s;
+        z *= s;
+        return (*this);
+    }
+
+    Vector3D& operator /=(float s)
+    {
+        s = 1.0F / s;
+        x *= s;
+        y *= s;
+        z *= s;
+        return (*this);
+    }
+
+    Vector3D& operator +=(const Vector3D& v)
+    {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        return (*this);
+    }
+
+    Vector3D& operator -=(const Vector3D& v)
+    {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        return (*this);
+    }
 };
 
+inline Vector3D operator +(const Vector3D& a, const Vector3D& b)
+{
+    return (Vector3D(a.x + b.x, a.y + b.y, a.z + b.z));
+}
+
+inline Vector3D operator -(const Vector3D& a, const Vector3D& b)
+{
+    return (Vector3D(a.x - b.x, a.y - b.y, a.z - b.z));
+}
+
+inline Vector3D operator *(const Vector3D& v, float s)
+{
+    return (Vector3D(v.x * s, v.y * s, v.z * s));
+}
+
+inline Vector3D operator /(const Vector3D& v, float s)
+{
+    s = 1.0F / s;
+    return (Vector3D(v.x * s, v.y * s, v.z * s));
+}
+
+// Negation not subtraction.
+inline Vector3D operator -(const Vector3D& v)
+{
+    return (Vector3D(-v.x, -v.y, -v.z));
+}
+
+inline float Magnitude(const Vector3D& v)
+{
+    return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
+}
+
+inline Vector3D Normalize(const Vector3D& v)
+{
+    return (v / Magnitude(v));
+}
 
 int main()
 {
