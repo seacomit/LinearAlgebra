19c17b9e2c16cc7af42b041011f0541814aa3b4b
Jeremy Blouir <jrmyblouir@gmail.com>
2023-06-05 10:04:45 -0600

    Add a 3x3 matrix implementation

--- LinearAlgebra.cpp
+++ LinearAlgebra.cpp
@@ -98,6 +98,51 @@ inline Vector3D Normalize(const Vector3D& v)
     return (v / Magnitude(v));
 }
 
+// A 3x3 matrix whose API uses row vectors, but under the hood converts to and works with column vectors.
+struct Matrix3D
+{
+    private:
+        float n[3][3];
+    public:
+        Matrix3D() = default;
+
+        Matrix3D(float n00, float n01, float n02,
+                 float n10, float n11, float n12,
+                 float n20, float n21, float n22)
+        {
+            n[0][0] = n00; n[0][1] = n10; n[0][2] = n20;
+            n[1][0] = n01; n[1][1] = n11; n[1][2] = n21;
+            n[2][0] = n02; n[2][1] = n12; n[2][2] = n22;
+        }
+
+        Matrix3D(const Vector3D& a, const Vector3D& b, const Vector3D& c)
+        {
+            n[0][0] = a.x; n[0][1] = a.y; n[0][2] = a.z;
+            n[1][0] = b.x; n[1][1] = b.y; n[1][2] = b.z;
+            n[2][0] = c.x; n[2][1] = c.y; n[2][2] = c.z;
+        }
+
+        float& operator ()(int i, int j)
+        {
+            return (n[j][i]);
+        }
+
+        const float& operator ()(int i, int j) const
+        {
+            return (n[j][i]);
+        }
+
+        Vector3D& operator [](int j)
+        {
+            return (*reinterpret_cast<Vector3D*>(n[j]));
+        }
+
+        const Vector3D& operator [](int j) const
+        {
+            return (*reinterpret_cast<const Vector3D*>(n[j]));
+        }
+};
+
 int main()
 {
     VideoMode vm(1920, 1080);
